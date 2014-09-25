9f010a764a0cd87a0d3a79f6f40848c802c6d01c
ben <benrxv@gmail.com>
2014-09-24 17:20:34 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import django_utils
+
+setup(name='Django Utilities',
+      version='0.0.1',
+      description='My custom utils for django',
+      author='Benjamin Rosnick',
+      author_email='benrxv@gmail.com',
+      url='https://github.com/benrxv/django-utils',
+      packages=['django-utils']
+)
